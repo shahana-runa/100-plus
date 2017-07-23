4e624d58bc8579d57b95a35bc8e36bb88b14d319
shahana-runa <ssaarruna@gmail.com>
2017-07-24 00:03:06 +0600

    class 4 creat

---
+++ Class 4/assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+
+   $(".team-carousel").owlCarousel({
+  	   items: 3,
+  	   margin: 30,
+  	   loop: true,
+  	   autoplay: true,
+  	   autoplayTimeout: 2000,
+  	   nav: true,
+  	   navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+  });
+
+});
