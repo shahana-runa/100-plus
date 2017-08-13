23ca45e4728ad3e705284e825eb190fc1bffd29e
shahana-runa <ssaarruna@gmail.com>
2017-08-14 05:38:49 +0600

    class 22 added

---
+++ class 22/assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+
+   $(".portfolio-items").owlCarousel({
+  	   items: 4,
+  	   loop: true,
+  	   autoplay: true,
+  	   autoplayTimeout: 2000,
+  	   dots: true
+  });
+
+});
