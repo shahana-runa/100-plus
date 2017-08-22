f6e5d54a95d6e944d0159637bf3102d6101e67ca
shahana-runa <ssaarruna@gmail.com>
2017-08-22 09:01:04 +0600

    class 29 added

---
+++ class 29/assets/js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($){
+	$(".protfolio-list").masonry({
+
+		// itemSelector: '.single-protfolio-item',
+		// columnWidth: 1,
+		// percentPosition: true
+		
+	});
+
+	$(".single-protfolio-item").hover(function() {
+		$(".single-protfolio-item .protfolio-hover h2").removeClass("animated fadeInUp");
+		$(this).find(".protfolio-hover h2").addClass("animated fadeInUp");
+		
+		$(".single-protfolio-item .protfolio-hover p").removeClass("animated fadeInUp");
+		$(this).find(".protfolio-hover p").addClass("animated fadeInUp");
+	});
+
+	$(".menu-trigger").on('click', function() {
+		$(".offcanvas-menu").addClass("active");
+		$(".canvas-overlay").addClass("active");
+	});
+
+
+	$(".menu-close i.fa, .canvas-overlay").on('click', function() {
+		$(".offcanvas-menu").removeClass("active");
+		$(".canvas-overlay").removeClass("active");
+	});
+
+});
