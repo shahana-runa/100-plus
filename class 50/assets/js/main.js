532865c0de26600c5dab8dd41f0b8c3302f1d956
shahana-runa <ssaarruna@gmail.com>
2017-11-26 10:22:49 +0600

    class 50 added

---
+++ class 50/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$(".slider-area").owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		dots: true,
+		nav: false
+	});
+});
